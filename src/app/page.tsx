a57ffcf0fa01ca519c931a9000095548af492677
wacky-sjh <sjh@wacky.kr>
2025-06-24 10:03:30 +0900

    Refact: 이미지 브리핑 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import ImageBriefing from "./components/ImageBriefing";
 
 export default function Home() {
   return (
-    <div>
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-pink-100 via-blue-100 to-violet-100 p-4">
       <ImageBriefing />
-    </div>
+    </main>
   );
 }
